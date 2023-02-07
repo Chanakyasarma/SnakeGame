b8485e21f676857d473ecbdea8d0adcebc1042de
Chanakya Sarma <chanakyasarmakashyap@gmail.com>
2023-02-07 16:19:30 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -10,7 +10,15 @@ let snakeArr=[
 ]
 food = {x:6, y:7};
 let score=0;
-
+function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
+    else document.getElementById('ac-wrapper').removeAttribute('style');
+}
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+    }, 10);
+}
 function main(ctime){
     window.requestAnimationFrame(main);
     // console.log(ctime);
